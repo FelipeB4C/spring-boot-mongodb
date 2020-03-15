425f58033074830960290e74831968803ae59c8d
Felipe Barros <felipeb4c@gmail.com>
2020-03-15 19:32:53 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/felipe/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/felipe/workshopmongo/repository/UserRepository.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.felipe.workshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String> {
 
-	
-	
 }
